b14dd05112d361a62f27c0d9a608ce67fcee2de1
Ray Vernagus <r.vernagus@gmail.com>
2022-03-31 00:51:18 +0000

    Add Dapr client and state code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-using Microsoft.AspNetCore.Builder;
+using Dapr.Client;
+
+const string storeName = "statestore";
+const string key = "counter";
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello!");
+app.MapGet("/", async () => 
+{
+    var daprClient = new DaprClientBuilder().Build();
+    var counter = await daprClient.GetStateAsync<int>(storeName, key);
+    await daprClient.SaveStateAsync(storeName, key, counter+1);
+    return counter;
+});
 
 await app.RunAsync();
